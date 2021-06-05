fb8091acef0c6f524ad4e235f86de0d97becdaf2
Weslley de Campos <weslleydrum@hotmail.com>
2021-06-05 15:38:56 -0300

    test: ensure SingUpController returns 500 if EmailValidator throw

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -2,6 +2,7 @@ import { SignUpController } from './singup'
 import { MissingParamError } from '../errors/missingParamError'
 import { InvalidParamError } from '../errors/invalidParamError'
 import { EmailValidator } from '../protocols/email-validator'
+import { ServerError } from '../errors/serverError'
 
 interface SutTypes {
     sut : SignUpController
@@ -93,7 +94,7 @@ describe('SingUp Controller', () =>{
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new InvalidParamError('email'))
     })
-     test('Should call email validator with correct email', () => {
+    test('Should call email validator with correct email', () => {
         const { sut, emailValidatorStub } = makeSut();
         
         const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
@@ -108,5 +109,27 @@ describe('SingUp Controller', () =>{
        
         sut.handle(httpRequest)
         expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
+    })
+    test('Should be return 500 if email validator throws', () => {
+        class EmailValidatorStub implements EmailValidator  {
+            isValid(email: string): boolean {
+                throw new Error()
+            }
+        }
+        const emailValidatorStub = new EmailValidatorStub()
+        const sut = new SignUpController(emailValidatorStub)
+        
+       
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                email:'any_email@mail.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(500)
+        expect(httpResponse.body).toEqual(new ServerError())
     })
 })
