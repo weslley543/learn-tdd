14587a60b3b1206b6730f1b626d02984f0cb51f4
Weslley de Campos <weslleydrum@hotmail.com>
2021-06-06 12:02:27 -0300

    test: ensure email validator adapter calls validator with correct email

--- src/presentation/utils/email-validator-adapter.spec.ts
+++ src/presentation/utils/email-validator-adapter.spec.ts
@@ -14,9 +14,15 @@ describe("Email Validator Adapter", () => {
         const isValid = sut.isValid('invalid_email@mail.com')
         expect (isValid).toBe(false)
     })
-     test('should return false if validator returns false', () =>{
+    test('should return false if validator returns false', () =>{
         const sut = new EmailValidatorAdapter();
         const isValid = sut.isValid('invalid_email@mail.com');
         expect (isValid).toBe(true)
+    })
+    test('should call validator with correct email', () =>{
+        const sut = new EmailValidatorAdapter();
+        const isSpyMail = jest.spyOn(validator, 'isEmail')
+        sut.isValid('any_email@mail.com');
+        expect (isSpyMail).toHaveBeenCalledWith('any_email@mail.com')
     })
 })
