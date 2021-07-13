d038a69fc089529d04fb37604e9d997bb6eb86f2
Weslley de Campos <weslleydrum@hotmail.com>
2021-07-12 22:14:38 -0300

    feat: ensure middleware works

--- src/main/middlewares/cors.ts
+++ src/main/middlewares/cors.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
 
-export const cors = (req, res, next) => {
+export const cors = (req: Request, res: Response, next: NextFunction) => {
     res.set('access-control-allow-origin', '*')
     res.set('access-control-allow-methods', '*')
     res.set('access-control-allow-headers', '*')
