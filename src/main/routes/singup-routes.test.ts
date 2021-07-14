e437a4defbcdb4eddc8c26e5440bf2e84af3fe78
Weslley de Campos <weslleydrum@hotmail.com>
2021-07-13 21:50:39 -0300

    feat: ensure mongo helper initializing

--- src/main/routes/singup-routes.test.ts
+++ src/main/routes/singup-routes.test.ts
@@ -1,7 +1,19 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/mongodb/helpers/mongo-helper'
 
 describe('SingUp Routes', () => {
+    beforeAll(async () => {
+        console.log(process.env.MONGO_URL)
+        await MongoHelper.connect(process.env.MONGO_URL)
+    })
+    afterAll(async () => {
+        await MongoHelper.disconnect()
+    })
+    beforeEach(async () => {
+        const accountCollection = await MongoHelper.getCollection('accounts')
+        await accountCollection.deleteMany({})
+    })
     test('should return an account on success ', async () => {
         
         await request(app)
