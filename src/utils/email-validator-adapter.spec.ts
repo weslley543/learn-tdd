bf35b5532503ed8d6a58cd681f95a46c9d606bcd
Weslley de Campos <weslleydrum@hotmail.com>
2021-07-18 17:29:05 -0300

    refactor: set a correct name for teste

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -18,7 +18,7 @@ describe("Email Validator Adapter", () => {
         const isValid = sut.isValid('invalid_email@mail.com')
         expect (isValid).toBe(false)
     })
-    test('should return false if validator returns false', () =>{
+    test('should return true if validator returns true', () =>{
         const sut = makeSut();
         const isValid = sut.isValid('invalid_email@mail.com');
         expect (isValid).toBe(true)
